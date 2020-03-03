ab4e2291f2222d54570a1fe8b3e0c1b87e96a299
rvarun7777 <rvarun7777@gmail.com>
2020-03-03 22:23:59 +0100

    Task: Add Two Numbers

---
+++ Medium/add_two_numbers.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    @staticmethod
+    def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = (l1.val if l1 else 0)
+            val2 = (l2.val if l2 else 0)
+            carry, out = divmod(val1 + val2 + carry, 10)
+
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next
+
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+
+        return result.next
+
+
+l1 = ListNode(128)
+l2 = ListNode(133)
+add = Solution
+add.add_two_numbers(l1, l2)
